a1c77774563d1e5e1e855b52c27268ade46b34e5
Ausn <AusnUnia@gmail.com>
2022-12-11 17:49:44 +0800

    2

--- thread_pool/thread_pool.cc
+++ thread_pool/thread_pool.cc
@@ -1,1 +1,83 @@
 #include"thread_pool.h"
+#include<iostream>
+
+template<class T>
+ThreadPool<T>::ThreadPool()
+: max_task_num_(20), task_queue_(20), thread_num_(4), threads_(thread_num_)
+{
+    for(int i=0;i<thread_num_;i++)
+    {
+        threads_[i]=new std::thread(worker_thread_entry_,this);
+        threads_[i]->detach();
+    }
+}
+
+template<class T>
+ThreadPool<T>::ThreadPool(int max_task_num, int thread_num)
+:max_task_num_(max_task_num),thread_num_(thread_num)
+{
+    if(thread_num_<=0||max_task_num_<=0)
+    {
+        std::cerr<<"Thread number and task number must be larger than 0\n";
+        throw std::exception();
+    }
+    
+    threads_.resize(thread_num_);
+
+    for(int i=0;i<thread_num_;i++)
+    {
+        threads_[i]=new std::thread(worker_thread_entry_,this);
+        threads_[i]->detach();
+    }
+}
+
+template<class T>
+ThreadPool<T>::~ThreadPool()
+{
+
+}
+
+template<class T>
+bool ThreadPool<T>::AddTask(T* task)
+{
+    std::lock_guard<std::mutex> guard(task_queue_mutex_);
+    if(task_queue_.size()>=max_task_num_)
+    {
+        std::cerr<<"Task_queue_ is full\n";
+        return false;
+    }
+
+    task_queue_.push(task);
+    return true;
+}
+
+
+template<class T>
+ThreadPool<T>* ThreadPool<T>::worker_thread_entry_(ThreadPool<T>* this_of_thread_pool)
+{
+    this_of_thread_pool->run();
+    return this_of_thread_pool;
+}
+
+template<class T>
+void ThreadPool<T>::run()
+{
+    while(1)
+    {
+        std::lock_guard<std::mutex> tmp_lock_guard(task_queue_mutex_);
+        if(task_queue_.empty())
+        {
+            continue;
+        }
+    }
+}
+
+
+int main()
+{
+    class task
+    {
+        
+    };
+    ThreadPool<task> tp(1,-1);
+}
--- thread_pool/thread_pool.h
+++ thread_pool/thread_pool.h
@@ -4,54 +4,28 @@
 #include<queue>
 #include<thread>
 #include<mutex>
+#include<vector>
+#include<exception>
 
 template<class T>
 class ThreadPool
 {
     public:
         ThreadPool();
+        ThreadPool(int max_task_num, int thread_num);
         ~ThreadPool();
+
+        bool AddTask(T* task);
     private:
-        ThreadPool<T>* worker_thread_entry_(ThreadPool<T>*); //the start function of worker thread.
+        static ThreadPool<T>* worker_thread_entry_(ThreadPool<T>*); //worker线程的入口函数，必须申明为static，因为初始化的时候就会调用该函数。
         void run();
     private:
-        int max_task_num_; //the maximum number of tasks in task_queue_.
-        std::queue<T> task_queue_; //a queue where tasks waiting to be executed by a thread.
-        int thread_num_; //the number of threads.
-        std::thread* threads_; //a pointer which points to the array of threads.
-        std::mutex task_queue_lock_; //a lock to protect task_queue_.
+        int max_task_num_; //task_queue_中能存在的最大任务数量
+        std::queue<T*> task_queue_; //等待被线程执行的任务队列
+        int thread_num_; //线程数量
+        std::vector<std::thread*> threads_; //装有所有线程的vector
+        std::mutex task_queue_mutex_; //用来保护task_queue_lock的互斥量
 };
 
-template<class T>
-ThreadPool<T>::ThreadPool(): max_task_num_(20), task_queue_(20), thread_num_(4)
-{
-    threads_=new std::thread[thread_num_];
-    for(int i=0;i<thread_num_;i++)
-    {
-        threads_[i]=std::move(std::thread(worker_thread_entry_,this));
-        threads_[i].detach();
-    }
-}
-
-template<class T>
-ThreadPool<T>* ThreadPool<T>::worker_thread_entry_(ThreadPool<T>* this_of_thread_pool)
-{
-    this_of_thread_pool->run();
-    return this_of_thread_pool;
-}
-
-template<class T>
-void ThreadPool<T>::run()
-{
-    while(1)
-    {
-        std::lock_guard<std::mutex> tmp_lock_guard(task_queue_lock_);
-        if(task_queue_.empty())
-        {
-            tmp_lock_guard.~lock_guard();
-            continue;
-        }
-    }
-}
 
 #endif
