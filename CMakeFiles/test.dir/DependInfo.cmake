5db22be9439e44ff755434e2db2b8600f2969faa
Ausn <AusnUnia@gmail.com>
2023-03-08 16:59:56 +0800

    2023 8.1

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ausn/Desktop/Server/mysql_connection_pool/mysql_connection_pool.cpp" "CMakeFiles/test.dir/mysql_connection_pool/mysql_connection_pool.cpp.o" "gcc" "CMakeFiles/test.dir/mysql_connection_pool/mysql_connection_pool.cpp.o.d"
+  "/home/ausn/Desktop/Server/test.cpp" "CMakeFiles/test.dir/test.cpp.o" "gcc" "CMakeFiles/test.dir/test.cpp.o.d"
+  "/home/ausn/Desktop/Server/timer/timer.cpp" "CMakeFiles/test.dir/timer/timer.cpp.o" "gcc" "CMakeFiles/test.dir/timer/timer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/mysql_connection_pool/mysql_connection_pool.cpp.o"
+  "CMakeFiles/test.dir/mysql_connection_pool/mysql_connection_pool.cpp.o.d"
+  "CMakeFiles/test.dir/test.cpp.o"
+  "CMakeFiles/test.dir/test.cpp.o.d"
+  "CMakeFiles/test.dir/timer/timer.cpp.o"
+  "CMakeFiles/test.dir/timer/timer.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
