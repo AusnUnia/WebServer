c48558bc0267b4219cc0573b864f170b414cd372
Ausn <AusnUnia@gmail.com>
2023-03-22 17:14:10 +0800

    2023-03-22 8.7.2

--- http/http_connection.cpp
+++ http/http_connection.cpp
@@ -1,6 +1,17 @@
 #include "http_connection.h"
 
 
+const std::string ok_200_title = "OK";
+const std::string error_400_title = "Bad Request";
+const std::string error_400_form = "Your request has bad syntax or is inherently impossible to staisfy.\n";
+const std::string error_403_title = "Forbidden";
+const std::string error_403_form = "You do not have permission to get file form this server.\n";
+const std::string error_404_title = "Not Found";
+const std::string error_404_form = "The requested file was not found on this server.\n";
+const std::string error_500_title = "Internal Error";
+const std::string error_500_form = "There was an unusual problem serving the request file.\n";
+
+
 int HttpConnection::epoll_fd_{-1};
 int HttpConnection::user_count_{0};
 
--- http/http_connection.h
+++ http/http_connection.h
@@ -2,16 +2,23 @@
 #define HTTP_CONNECTION_H
 
 #include <netinet/in.h>
-#include <unistd.h>
+#include <unistd.h> 
+#include <fcntl.h>
 
 #include<iostream>
 #include<memory>
+#include<map>
 
 #include"../mysql_connection_pool/mysql_connection_pool.h"
+#include"../lock/my_semaphore.h"
+#include"../timer/timer.h"
 
 class HttpConnection
 {
 public:
+    static const int kFileNameLen{200};
+    static const int kReadBufferSize{2048};
+    static const int kWriteBufferSize{1024};
 
     enum class Method
     {
@@ -26,14 +33,14 @@ public:
         PATH
     };
 
-    enum CheckState
+    enum class CheckState
     {
         CHECK_STATE_REQUESTLINE = 0,
         CHECK_STATE_HEADER,
         CHECK_STATE_CONTENT
     };
 
-    enum HttpCode
+    enum class HttpCode
     {
         NO_REQUEST,
         GET_REQUEST,
@@ -45,14 +52,58 @@ public:
         CLOSED_CONNECTION
     };
 
-public:
-    void InitMysqlResult(std::shared_ptr<MysqlConnectionPool> connection_pool);
+    enum class LineStatus
+    {
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
 
+public:
+    HttpConnection(){}
+    ~HttpConnection(){}
+
+public:
     void Init(int sock_fd, const sockaddr_in &addr, std::string_view root, int trig_mode,
                      int close_log, std::string_view database_user, std::string_view database_password, std::string_view database_name);
+    void CloseConnection(bool real_close=true);
+    bool ReadOnce();
+    bool Write();
+
+    sockaddr_in* get_address()
+    {
+        return  &address_;
+    }
+
+    void InitMysqlResult(std::shared_ptr<MysqlConnectionPool> connection_pool);
+    int timer_flag_;
+    int improve_;
+
+
+private:
+    void Init();
+    HttpCode ProcessRead();
+    bool ProcessWrite(HttpCode ret);
+    HttpCode parse_request_line(char *text);
+    HttpCode parse_headers(char *text);
+    HttpCode parse_content(char *text);
+    HttpCode do_request();
+    char *get_line() { return (read_buffer_+start_line_); };
+    LineStatus parse_line();
+    void unmap();
+    bool add_response(const char *format, ...);
+    bool add_content(const char *content);
+    bool add_status_line(int status, const char *title);
+    bool add_headers(int content_length);
+    bool add_content_type();
+    bool add_content_length(int content_length);
+    bool add_linger();
+    bool add_blank_line();
+
 
 
     //////////////////////test/////////////////////////////////////
+public:
     HttpConnection(int sock):sock_(sock)
     {
 
@@ -92,6 +143,48 @@ public:
 public:
     static int epoll_fd_;
     static int user_count_;
+    std::shared_ptr<MYSQL> mysql_;
+    int state_; //读为0, 写为1
+
+private:
+    int sock_fd_;
+    sockaddr_in address_;
+
+    char read_buffer_[kReadBufferSize];
+    int read_idx_;
+    int checked_idx_;
+    int start_line_;
+
+    char write_buffer_[kWriteBufferSize];
+    int write_idx_;
+
+    CheckState check_state_;
+    Method method_;
+
+    char real_file_[kFileNameLen];
+    std::string url_;
+    std::string version_;
+    std::string host_;
+    int content_length_;
+    bool linger_;
+    std::string file_address_;
+    struct stat file_stat_;
+    struct iovec iovec_[2];
+    int iovec_count_;
+    int cgi_;        //是否启用的POST
+    std::string request_header_; //存储请求头数据
+    int bytes_to_send_;
+    int bytes_have_send_;
+    std::string doc_root_;
+
+    std::map<std::string,std::string> users_;
+    int trig_mode_;
+    int close_log_;
+
+    std::string database_user_;
+    std::string database_password_;
+    std::string database_name_;
+
 };
 
 
